ecd99999672f29b22cb45c76b848db1e9d5c4bfe
noah.zingler <noah.zingler@rockwellcollins.com>
2020-10-24 16:16:29 -0400

    Added rotational image capturing

--- nz_exercise_1.py
+++ nz_exercise_1.py
@@ -9,12 +9,18 @@ shape4 = box(pos=vector(1.5,2.5,0), size=vector(1,2,1), color=color.green)
 
 sample48 = compound([shape1, shape2, shape3, shape4])
 
-rotational = cylinder(pos=vector(0,-5,.5), axis=vector(0,10,0), radius=.1)
+
+#rotational = cylinder(pos=vector(0,-5,.5), axis=vector(0,10,0), radius=.1)
 #sample48.rotate(angle=45, axis=vec(1,1,1))
 #sample48.rotate(angle=45, axis=vector(0,10,0))
 
-sample48.rotate(angle=radians(90), axis=vector(0,10,0))
+# while True:
+# 	sleep(.1)
+# 	sample48.rotate(angle=radians(10), axis=vector(0,1,0))
 
-while True:
-	sleep(.1)
-	sample48.rotate(angle=radians(10), axis=vector(0,10,0))
+angles = [0, 45, 90, 135, 180]
+for i in angles:
+	sample48.rotate(angle=radians(i), axis=vector(0,1,0))
+	scene.capture("sample48_" + str(i))
+	sleep(1)
+	sample48.rotate(angle=radians(-i), axis=vector(0,1,0))
