66ec8d7bdc052d9e5944dbd68d3bfb8d37389bba
noah.zingler <noah.zingler@rockwellcollins.com>
2020-10-24 11:10:51 -0400

    Created first 3D shape rendering (sample shape #48)

---
+++ nz_exercise_1.py
@@ -0,0 +1,8 @@
+from vpython import *
+
+# This code renders shape 48 of the sample group with the segments in different colors
+
+box(pos=vector(0,1,0), size=vector(4,1,1), color=color.white)
+box(pos=vector(-1.5,-1,0), size=vector(1,3,1), color=color.blue)
+box(pos=vector(-1.5,-2,1.5), size=vector(1,1,2), color=color.red)
+box(pos=vector(1.5,2.5,0), size=vector(1,2,1), color=color.green)
